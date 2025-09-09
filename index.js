846289a2c158b40f9498719021704d2fc822e45d
Rafael Reis <rafyangtj@gmail.com>
2025-09-09 15:44:55 -0300

    updated-db

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
+
+mongoose
+   .connect(keys.mongoURI)
+   .then(() => console.log('✅ Conectado com sucesso!'))
+   .catch(err => console.error('❌ Erro de conexão:', err.message));
 
 const app = express();
 
@@ -7,4 +14,3 @@ require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-git
