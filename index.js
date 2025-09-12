7abdbf68c52edbde84d1972beb58ead9d4baea31
Rafael Reis <rafyangtj@gmail.com>
2025-09-12 14:22:05 -0300

    completed-atuh

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
+require('./models/user');
 require('./services/passport');
 
 mongoose
@@ -9,6 +12,17 @@ mongoose
    .catch(err => console.error('❌ Erro de conexão:', err.message));
 
 const app = express();
+
+app.use(
+   cookieSession({
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      keys: [keys.cookieKey]
+   })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 require('./routes/authRoutes')(app);
 
