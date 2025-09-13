36107146c42fc47a8de0cdb76acf021230e27f3d
Rafael Reis <rafyangtj@gmail.com>
2025-09-13 14:52:55 -0300

    new-enviroment

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+// prod.js - production keys here - do not commit this file to GitHub!
+
+module.exports = {
+	 googleClientID: process.env.GOOGLE_CLIENT_ID,
+	 googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+	 mongoURI: process.env.MONGO_URI,
+	 cookieKey: process.env.COOKIE_KEY
+};
