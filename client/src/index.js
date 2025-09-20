53d1d69d5ed749ae86b9809c453c35bf2e0577c2
Rafael Reis <rafyangtj@gmail.com>
2025-09-20 14:49:39 -0300

    updated-auth

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,17 @@
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './reducers';
+
+const store = configureStore({
+   reducer: rootReducer
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+
+root.render(
+   <Provider store={store}>
+      <App />
+   </Provider>
+);
---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default function rootReducer(state = null, action) {
+   switch (action.type) {
+      default:
+         return state;
+   }
+}
