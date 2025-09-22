5e632b05654c1645ddad71dc21d8605272366f43
Rafael Reis <rafyangtj@gmail.com>
2025-09-22 20:17:34 -0300

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,7 +8,13 @@ module.exports = app => {
       })
    );
 
-   app.get('/auth/google/callback', passport.authenticate('google'));
+   app.get(
+      '/auth/google/callback',
+      passport.authenticate('google'),
+      (req, res) => {
+         res.redirect('http://localhost:3000/surveys');
+      }
+   );
 
    app.get('/api/logout', (req, res, next) => {
       req.logout(err => {
