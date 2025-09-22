50d6df9c3f8f0fa56afb90a30d2a9d7c452ef1b8
Rafael Reis <rafyangtj@gmail.com>
2025-09-22 07:10:52 -0300

    added-fetchUser

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-const fetchUser = () => async dispatch => {
-   const res = await axios.get('/api/current_user');
-   dispatch({ type: 'FETCH_USER', payload: res.data });
+export const fetchUser = () => {
+   return async function (dispatch) {
+      const res = await axios.get('/api/current_user');
+      dispatch({ type: 'FETCH_USER', payload: res.data });
+   };
 };
