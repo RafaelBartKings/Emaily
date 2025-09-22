9808d0b28fdca3e5adea7b042ba5047d944bfa45
Rafael Reis <rafyangtj@gmail.com>
2025-09-22 16:51:37 -0300

    header-done

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,7 +1,11 @@
 // eslint-disable-next-line import/no-anonymous-default-export
+import { FETCH_USER } from '../actions/types';
+
 export default function rootReducer(state = null, action) {
    console.log(action);
    switch (action.type) {
+      case FETCH_USER:
+         return action.payload || false;
       default:
          return state;
    }
