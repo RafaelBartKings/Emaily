101f146584d71fec26e33e73631d37a37e8892fa
Rafael Reis <rafyangtj@gmail.com>
2025-09-27 15:15:53 -0300

    Deploy inicial com React + Express

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,5 @@
 // prod.js - production keys here - do not commit this file to GitHub!
 
-const { stripeSecretKey } = require('./dev');
-
 module.exports = {
    googleClientID: process.env.GOOGLE_CLIENT_ID,
    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
