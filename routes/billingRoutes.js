1a3542993e2c604d9f1632f185e56b200cdf60ff
Rafael Reis <rafyangtj@gmail.com>
2025-09-27 10:17:40 -0300

    added-require-login

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,8 +1,9 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
-   app.post('/api/stripe', async (req, res) => {
+   app.post('/api/stripe', requireLogin, async (req, res) => {
       try {
          const charge = await stripe.charges.create({
             amount: 500, // amount in cents
@@ -10,7 +11,10 @@ module.exports = app => {
             source: req.body.id, // token from client
             description: 'Emaily credits'
          });
-         res.send(charge);
+         //  res.send(charge);
+         req.user.credits += 5;
+         const user = await req.user.save();
+         res.send(user);
       } catch (err) {
          res.status(500).send({ error: err.message });
       }
