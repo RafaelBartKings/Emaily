95cb9093f10a3bf23e1711574b66238f43490edd
Rafael Reis <rafyangtj@gmail.com>
2025-09-29 09:13:03 -0300

    Recipient-Survey

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const path = require('path');
 
 require('./models/user');
 require('./services/passport');
+require('./models/Survey');
 
 mongoose
    .connect(keys.mongoURI)
@@ -51,7 +52,7 @@ if (process.env.NODE_ENV === 'production') {
          res.sendFile(path.resolve(__dirname, 'client', 'build', req.path));
       }
    );
-   
+
    // Catch-all: qualquer outra rota cai no React
    app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
