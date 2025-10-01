85d0973a8498bc363370d6d9f1b74ecc1279bbd1
Rafael Reis <rafyangtj@gmail.com>
2025-09-30 21:17:03 -0300

    Fix prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,5 @@
 // prod.js - production keys here - do not commit this file to GitHub!
-
-const { sendGridKey } = require('./dev');
+// Este arquivo DEVE usar APENAS as variáveis de ambiente configuradas no Vercel.
 
 module.exports = {
    googleClientID: process.env.GOOGLE_CLIENT_ID,
@@ -9,6 +8,9 @@ module.exports = {
    cookieKey: process.env.COOKIE_KEY,
    stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
    stripeSecretKey: process.env.STRIPE_SECRET_KEY,
-   googleCallbackURL: 'https://emaily-coral.vercel.app/auth/google/callback',
-   sendGridKey: process.env.SEND_GRID_KEY
+   // Note: O redirectDomain deve ser usado para construir o URL de callback
+   googleCallbackURL: process.env.REDIRECT_DOMAIN + '/auth/google/callback',
+   sendGridKey: process.env.SEND_GRID_KEY,
+   mailFrom: process.env.MAIL_FROM, // Chave que faltava para o SendGrid
+   redirectDomain: process.env.REDIRECT_DOMAIN
 };
